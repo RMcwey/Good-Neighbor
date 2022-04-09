26ba454b9679d6fb75f5ad503c3cd6812ba96565
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-04-09 11:50:33 -0400

    cleaned up the new borrow/return functions ... it all works!

--- public/js/movie.js
+++ public/js/movie.js
@@ -1,51 +1,38 @@
 const borrowMovie = async (event) => {
-  console.log("BORROW button was pressed");
-  
   if (event.target.hasAttribute('data-id')) {
-    console.log("button has a data-id attribute");
     const movie_id = event.target.getAttribute('data-id');
-    
-    console.log(movie_id);
 
-    await fetch(`/api/movies/${movie_id}`, { 
+    const response = await fetch(`/api/movies/${movie_id}`, { 
       method: 'PUT',
       body: JSON.stringify({'is_available': false}),
       headers: { 'Content-Type': 'application/json' },
     });
 
-    // console.log(response);
-
-  /* if (response.ok) {
-    // document.location.replace('/profile');
+  if (response.ok) {
+    document.location.replace('/profile');
   } else {
     alert('Failed to update movie.');
-  } */
+  }
 }};
 
 
-
 const returnMovie = async (event) => {
-  console.log("RETURNING button was pressed");
   if (event.target.hasAttribute('data-id')) {
-    console.log("button has a data-id attribute");
     const movie_id = event.target.getAttribute('data-id');
 
-    console.log(movie_id);
-
-    await fetch(`/api/movies/${movie_id}`, {
+    const response = await fetch(`/api/movies/${movie_id}`, {
       method: 'PUT',
       body: JSON.stringify({"is_available": true}),
       headers: { 'Content-Type': 'application/json' },
     });
 
-    // console.log(response);
-
-  /* if (response.ok) {
-    // document.location.replace('/profile');
+  if (response.ok) {
+    document.location.replace('/profile');
   } else {
     alert('Failed to update movie.');
-  } */
+  }
 }};
+
 
 document
   .querySelector('#return-button')
